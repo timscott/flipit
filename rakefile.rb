fca5da746dcce9828679fa77553c2df9a7cc550a
Tim Scott <tscott@lunaversesoftware.com>
2012-02-20 15:49:23 -0600

    Added packaing with nuget.

--- rakefile.rb
+++ rakefile.rb
@@ -3,17 +3,24 @@ require 'albacore'
 require 'fileutils'
 
 PRODUCT_NAME = "FlipIt"
-SOURCE_PATH = File.expand_path "src"
-OUTPUT_PATH = File.expand_path "output"
-TESTS_PATH = "#{SOURCE_PATH}/#{PRODUCT_NAME}.Tests"
+BASE_PATH = File.expand_path ""
+SOURCE_PATH = "#{BASE_PATH}/src/"
+OUTPUT_PATH = "#{BASE_PATH}/output/"
+TESTS_PATH = "#{SOURCE_PATH}/#{PRODUCT_NAME}.Tests/bin/Release/"
 TESTS_OUTPUT_PATH = "#{OUTPUT_PATH}/tests"
+PACKAGE_OUTPUT_PATH = "#{OUTPUT_PATH}/package/"
+PACKAGE_OUTPUT_LIB_PATH = "#{PACKAGE_OUTPUT_PATH}/lib/"
 TEST_REPORT_PATH =  "#{OUTPUT_PATH}/report"
 COMMON_ASSEMBY_INFO_FILE = "#{SOURCE_PATH}/CommonAssemblyInfo.cs"
 MSPEC_VERSION = "0.5.3.0"
+VERSION = "0.1.0.0"
+PACKAGE_PATH = "#{BASE_PATH}/package"
+NUSPEC_FILENAME = "#{PRODUCT_NAME}.nuspec"
+NUSPEC_PATH = "#{PACKAGE_PATH}/#{NUSPEC_FILENAME}"
 
-task :default => [:full]
-
-task :full => [:assemblyInfo,:build,:copy_for_test,:test]
+task :default => [:standard]
+task :standard => [:assemblyInfo,:build,:output,:test,:define_package]
+task :package => [:standard,:create_package]
 
 msbuild :build do |msb|
 	msb.properties :configuration => :Release
@@ -22,12 +29,15 @@ msbuild :build do |msb|
 	msb.solution = "#{SOURCE_PATH}/#{PRODUCT_NAME}.sln"
 end
 
-task :copy_for_test do
+task :output do
 	FileUtils.rmtree OUTPUT_PATH
 	FileUtils.mkdir OUTPUT_PATH
 	FileUtils.mkdir TEST_REPORT_PATH
 	FileUtils.mkdir TESTS_OUTPUT_PATH
-	FileUtils.cp_r "#{TESTS_PATH}/bin/Release/.", TESTS_OUTPUT_PATH
+	FileUtils.mkdir PACKAGE_OUTPUT_PATH
+	FileUtils.mkdir PACKAGE_OUTPUT_LIB_PATH
+	FileUtils.cp_r "#{TESTS_PATH}.", TESTS_OUTPUT_PATH
+	copy_files TESTS_PATH, PACKAGE_OUTPUT_LIB_PATH, PRODUCT_NAME, ["dll", "pdb", "xml"]
 end
 
 mspec :test do |mspec|
@@ -43,7 +53,43 @@ assemblyinfo :assemblyInfo do |asm|
 	asm.title = PRODUCT_NAME
 	asm.company_name = "Lunaverse Software"
 	asm.product_name = PRODUCT_NAME
-	asm.version = "0.1.0.0"
-	asm.file_version = "0.1.0.0"
+	asm.version = VERSION
+	asm.file_version = VERSION
 	asm.copyright = "Copyright (c) 2011 Lunaverse Software"
+end
+
+desc "Create the FlipIt nuspec file"
+nuspec :define_package do |nuspec|
+	version = "#{ENV['version']}"
+	nuspec.id = "FlipIt"
+	nuspec.version = version.length == 7 ? version : VERSION
+	nuspec.authors = "Tim Scott"
+	nuspec.owners = "Tim Scott"
+	nuspec.description = "A feature flipper. FlipIt provides a simple and flexible way to flip features in a .NET application."
+	nuspec.title = "FlipIt"
+	nuspec.language = "en-US"
+	nuspec.licenseUrl = "https://github.com/timscott/flipit/blob/master/LICENSE.TXT"
+	nuspec.projectUrl = "https://github.com/timscott/flipit/wiki"
+	nuspec.tags = "FeatureFlipper Deployment Settings Configuration"
+	nuspec.working_directory = PACKAGE_PATH
+	nuspec.output_file = NUSPEC_FILENAME
+end
+
+desc "Create the nuget package"
+task :create_package do
+    cmd = Exec.new
+    cmd.command = 'tools/nuget.exe'
+    cmd.parameters = "pack #{NUSPEC_PATH} -basepath #{PACKAGE_OUTPUT_PATH} -outputdirectory #{PACKAGE_PATH}"
+    cmd.execute
+end
+
+def copy_files(from, to, filename, extensions)
+  extensions.each do |ext|
+	from_name = "#{from}#{filename}.#{ext}"
+	to_name = "#{to}#{filename}.#{ext}"
+	if File.exists? from_name
+		puts "Copying '#{from_name}' to '#{to_name}'"
+		FileUtils.cp from_name, to_name
+	end
+  end
 end
